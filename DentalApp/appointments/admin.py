85f781fa8cc6bdfe14f38d7dbfaa8d07b307c368
Mtumishi <blakhite@gmail.com>
2024-09-20 11:04:18 -0400

    admin

--- DentalApp/appointments/admin.py
+++ DentalApp/appointments/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Service
 
-# Register your models here.
+
+
+admin.site.register(Service)
+
+
+
+
+"""Registered Service model so it appears in the admin dashboard. 
+This will enable me to add the dental services using the admin dashboard"""
